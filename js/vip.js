0b9b72c5f383f69c94d34e4f8c483d0cc322acfd
Joe <39517873+wurom@users.noreply.github.com>
2020-04-14 18:01:48 +0800

    Add files via upload

--- js/vip.js
+++ js/vip.js
@@ -1,6 +1,4 @@
 
-var tvobj  = document.getElementById("tvv");
-
 var VIP_INFO =new Array(
 ["思古","https://api.sigujx.com/jx/?url="],
 ["金桥","http://jqaaa.com/jx.php?url="],
@@ -25,16 +23,25 @@ var VIP_INFO =new Array(
 
 );
 
+var see = document.getElementById("see");
+var rid = 0 ;
+var tvobj  = document.getElementById("tvv");
+var Furl   = "";
+var TvInfo = document.getElementById("play");
+var TvJSON = {};
 
+//检测网址
 function IsURL(strUrl) {
-    var regular = /^\b(((https?|ftp):\/\/)?[-a-z0-9]+(\.[-a-z0-9]+)*\.(?:com|edu|gov|int|mil|net|org|biz|info|name|museum|asia|coop|aero|[a-z][a-z]|((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]\d)|\d))\b(\/[-a-z0-9_:\@&?=+,.!\/~%\$]*)?)$/i
-    if (regular.test(strUrl)) {
-        return true;
-    }
-    else {
-        return false;
-    }
+	var regular = /^\b(((https?|ftp):\/\/)?[-a-z0-9]+(\.[-a-z0-9]+)*\.(?:com|edu|gov|int|mil|net|org|biz|info|name|museum|asia|coop|aero|[a-z][a-z]|((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]\d)|\d))\b(\/[-a-z0-9_:\@&?=+,.!\/~%\$]*)?)$/i
+	return regular.test(strUrl)
 }
+//跨域权限
+$.ajaxPrefilter(function (options) {
+	if (options.crossDomain && jQuery.support.cors) {
+		var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+		options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+	}
+});
 
 function GetSel(){
 	var SLIST = new Array("https://v.qq.com/x/search/","https://so.iqiyi.com/","https://www.soku.com/","https://so.mgtv.com/so","about:blank");
@@ -47,74 +54,106 @@ function GetSel(){
 		}
 	}
 }
-//<div><iframe id="ts" name="ts" height=200 width=100% src="https://v.qq.com/x/search/"></iframe></div>
+
 function GoUrl() {
 	var url = document.getElementById("url").value;
 	tvobj.innerHTML = '<iframe name="tvif" height=400 width=100%  src="'+url+'"> </iframe>' ;
 }
 
-var see = document.getElementById("see");
-var rid = 0 ;
-//while (VIP_INFO[z++])
-function NoCheck() {
-	see[rid].checked=0;
-}
-for (var z=0;z < VIP_INFO.length ;z++){
-	see.innerHTML += '<label><input name="seer" type="radio"/>'+VIP_INFO[z][0]+'</label>';
-}
 
-$.ajaxPrefilter(function (options) {
-	if (options.crossDomain && jQuery.support.cors) {
-		var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
-		options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
-		//options.url = "http://cors.corsproxy.io/url=" + options.url;
+if(see){
+	for (var z=0;z < VIP_INFO.length ;z++){
+		see.innerHTML += '<label><input name="seer" type="radio"/>'+VIP_INFO[z][0]+'</label>';
 	}
-});
-
+}
 
 function GetCode(){
 	var url  = document.getElementById("url").value;
-	if(url){
+	var tvobj = document.getElementById("code");
+	if(IsURL(url)){
 		tvobj.innerHTML = "Load...";
-		var code = null;
+		//var code = null;
 		$.get(url,function (response) {
 			//$("#html").html(response);
-			var mydata = response.match(new RegExp('LIST_INFO'+'(.*?)'+'data'))[1];
-			mydata = mydata.substring(mydata.indexOf("[") + 1, mydata.indexOf("]"));
+			//var mydata = response.match(new RegExp('LIST_INFO'+'(.*?)'+'data'))[1];
+			//mydata = mydata.substring(mydata.indexOf("[") + 1, mydata.indexOf("]"));
 
-			tvobj.innerHTML = '<textarea rows="10" style="width:100%"/>'+mydata+'</textarea>';//.match(new RegExp('["'+'(.*?)'+'"]'))[1];
+			tvobj.innerHTML = '<textarea rows="20" style="width:100%"/>'+response+'</textarea>';//.match(new RegExp('["'+'(.*?)'+'"]'))[1];
 
-			code = new Array(mydata.replace(/\"/g, "").split(','));
+			//code = new Array(mydata.replace(/\"/g, "").split(','));
 
+		}).fail(function () {
+			tvobj.innerHTML = '查看失败';
 		});
 	}
+	else
+	{
+		tvobj.innerHTML = '请输入正确网址';
+	}
 }
-//https://v.qq.com/x/cover/mpytzuh6n4ku1cs/b0014ppwn7r.html
-function SeeMe() {
-	var selurl = null;
-	var url  = document.getElementById("url").value;
-	//var code = new Array(document.getElementById("codet").value);
-	//var str = document.getElementById("codet").value;
-	//var code = new Array(str.replace(/\"/g, "").split(','));
-	var tv1 = document.getElementById("tv1");
 
+function SeeMe() {
 	for(var i =0 ;i<see.length;i++){
 		if(see[i].checked){
 			//alert(iframe[i].value);
-			selurl = VIP_INFO[i][1];
+			//selurl = VIP_INFO[i][1];
 			rid = i;
 			break;
 		}
 	}
 	
+	if(!TvJSON.ok)
+	return;
+
+	if(TvJSON.len>1){
+		TvInfo.innerHTML = "";
+		for (var zz=0;zz < TvJSON.len ;zz++)
+		{
+			if(TvJSON.data[zz].mask == 1 || TvJSON.data[zz].mask == 7)
+			TvInfo.innerHTML += '<span><a target="_blank" href="'+VIP_INFO[rid][1]+TvJSON.data[zz].url+'">'+ (zz+1) + 'v</a></span>';
+			else if(TvJSON.data[zz].mask != 4)
+			TvInfo.innerHTML += '<span><a target="_blank" href="'+VIP_INFO[rid][1]+TvJSON.data[zz].url+'">'+ (zz+1) + '</a></span>';
+		}
+	}
+	else
+	TvInfo.innerHTML = '<a target="_blank" href="'+VIP_INFO[rid][1]+TvJSON.data[0].url+'">点我播放</a>';
+
+	//document.getElementById("tvv").innerHTML = '<iframe name="tvif" height=200 width=100%  src="'+SLIST[i]+'"> </iframe>' ;
+	//document.getElementById("mv").innerHTML = '<video src="'+selurl+'" width="320" height="240" controls="controls"></video>';
+}
+
+//网址有变化时
+function NoCheck() {
+	var url  = document.getElementById("url").value;
+	//var url  = "https://v.qq.com/x/cover/mzc00200a7ghbkc.html";
 	if(!IsURL(url)){
-	  tv1.innerHTML = '请输入正确网址';	
-	  return;
+		TvInfo.innerHTML = '请输入正确网址';
+		return;
+	}
+	TvJSON={};
+			//window.open(selurl+url);
+			TvJSON.data = new Array(1);
+			TvJSON.data[0]={};
+			TvJSON.data[0].url  = url;
+			TvJSON.ok = 1;
+			
+if(see[rid].checked)
+		SeeMe();
+	}
+//剧集
+function GetAll() {
+	var url  = document.getElementById("url").value;
+	//var url  = "https://v.qq.com/x/cover/mzc00200a7ghbkc.html";
+	if(!IsURL(url)){
+		TvInfo.innerHTML = '请输入正确网址';
+		return;
 	}
 	
-	if(url.search("qq.com") != -1){
-		tv1.innerHTML = "Load...";
-		var code = null;
+  //see[rid].checked=1;
+  
+	if(url.search("qq.com") != -1){//https://v.qq.com/x/cover/m441e3rjq9kwpsc/l0025jj5k4c.html
+		TvInfo.innerHTML = "腾讯剧集解析中...";//https://v.qq.com/x/cover/dxd1v76tmu0wjuj.html
+		TvJSON={};
 		$.get(url,function (response) {
 			//$("#html").html(response);
 			//var mydata = response.match(new RegExp('LIST_INFO'+'(.*?)'+'data'))[1];
@@ -123,44 +162,63 @@ function SeeMe() {
 			//tvobj.innerHTML = '<textarea rows="10" style="width:100%"/>'+mydata+'</textarea>';//.match(new RegExp('["'+'(.*?)'+'"]'))[1];
 			//code = new Array(mydata.replace(/\"/g, "").split(','));
 			var mydata = response.match(new RegExp('update_des'+'(.*?)'+',"comment_show_type'))[1];
-      var jsonstr ='{"'+mydata+'}';
-		  var obj = JSON.parse(jsonstr);
-      var len = obj.vip_ids.length;
-      if(len > 1){
- 				tv1.innerHTML = "";
-				var index = url.lastIndexOf("/");
-				var codehead = url.substring(0, index+1);
-				     	
-				for (var zz=0;zz < len ;zz++)
+			var obj = JSON.parse('{"'+mydata+'}');
+			TvJSON.title = obj.title;
+			TvJSON.len =  obj.vip_ids.length;
+			TvJSON.data = new Array(TvJSON.len);
+			if(TvJSON.len > 1){
+				var codehead = url.substring(0, url.lastIndexOf("/")+1);
+
+				for (var zz=0;zz < TvJSON.len ;zz++)
 				{
-					if(obj.vip_ids[zz].F == 2)
-					tv1.innerHTML += '<span><a target="_blank" href="'+selurl+codehead+obj.vip_ids[zz].V+'.html">'+ (zz+1) + '</a></span>';
-					else if(obj.vip_ids[zz].F == 7)
-					tv1.innerHTML += '<span><a target="_blank" href="'+selurl+codehead+obj.vip_ids[zz].V+'.html">'+ (zz+1) + 'v</a></span>';	
-				}							
-			/*if(code[0].length > 1){
-				tv1.innerHTML = "";
-				var index = url.lastIndexOf("/");
-				var codehead = url.substring(0, index+1);
-
-				while (code[0][zz++]){
-					tv1.innerHTML += '<span><a target="_blank" href="'+selurl+codehead+code[0][zz-1]+'.html">'+ zz + '</a></span>';
-				}*/
+					TvJSON.data[zz]={};
+					TvJSON.data[zz].mask = obj.vip_ids[zz].F;
+					TvJSON.data[zz].url  = codehead+obj.vip_ids[zz].V+'.html';
+				}
+			}else{
+				var codehead = url.substring(0, url.lastIndexOf("."));
+				TvJSON.data[0]={};
+				TvJSON.data[0].mask = obj.vip_ids[0].F;
+				TvJSON.data[0].url  = codehead+'/'+obj.vip_ids[0].V+'.html';
 			}
-			else{
-	      window.open(selurl+url);
-	      tv1.innerHTML = '新窗口播放...';
-      }
-				
-		});
-	}
-	else
-	{
-	  window.open(selurl+url);
-	  tv1.innerHTML = '新窗口播放...';
-  }
-	//tv1.innerHTML = '<span><a target="_blank" href="'+selurl+url+'">'+ zz + '</a></span>';
+			TvJSON.ok = 1;
+			SeeMe();
 
-	//document.getElementById("tvv").innerHTML = '<iframe name="tvif" height=200 width=100%  src="'+SLIST[i]+'"> </iframe>' ;
-	//document.getElementById("mv").innerHTML = '<video src="'+selurl+'" width="320" height="240" controls="controls"></video>';
-}
+		  }).fail(function (){TvJSON.ok=null;TvInfo.innerHTML = '解析失败';});
+		}	
+		else if(url.search("iqiyi.com") != -1){//https://www.iqiyi.com/v_19rx9jx19s.html
+			TvInfo.innerHTML = "爱奇艺剧集解析中...";//http://www.iqiyi.com/v_19rrokkpxs.html
+			
+			$.get(url,function (response) {
+				var mydata = response.match(new RegExp('albumid'+'(.*?)'+'";'))[1];
+				mydata = mydata.split('"')[1];
+				TvInfo.innerHTML = "返回albumid:"+mydata;
+				if(mydata > 1){//https://pcw-api.iqiyi.com/albums/album/avlistinfo?aid=202328401&page=1&size=30
+				TvJSON={};
+				$.get('https://pcw-api.iqiyi.com/albums/album/avlistinfo?aid='+mydata+'&page=1&size=9999',function (res) {
+					var obj = res;
+					TvJSON.len = obj.data.epsodelist.length;
+					TvJSON.data = new Array(TvJSON.len);
+					if(TvJSON.len > 1){
+						for (var zz=0;zz < TvJSON.len ;zz++)
+						{
+							TvJSON.data[zz]={};
+							TvJSON.data[zz].mask = obj.data.epsodelist[zz].payMark;
+							TvJSON.data[zz].url  = obj.data.epsodelist[zz].playUrl;
+						}
+					}else{
+						TvJSON.data[0]={};
+						TvJSON.data[0].mask = obj.data.epsodelist[zz].payMark;
+						TvJSON.data[0].url  = obj.data.epsodelist[zz].playUrl;
+					}
+					
+					TvJSON.ok = 1;
+					SeeMe();
+
+			  });
+			}					
+					SeeMe();
+			}).fail(function (){TvJSON.ok=null;TvInfo.innerHTML = '解析失败';});
+		}
+
+ }
